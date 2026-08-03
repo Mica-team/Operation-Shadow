144dc4358a5274fe9d46fc27e3b485503f0eacd4
jimj90201-a11y <jimj90201@gmail.com>
2026-08-03 23:11:23 +0530

    Register mod blocks and items in constructor

--- src/main/java/com/gamerofpro/operationshadow/OperationShadow.java
+++ src/main/java/com/gamerofpro/operationshadow/OperationShadow.java
@@ -1,7 +1,11 @@
 package com.gamerofpro.operationshadow;
 
+import com.gamerofpro.operationshadow.block.ModBlocks;
+import com.gamerofpro.operationshadow.item.ModItems;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(OperationShadow.MOD_ID)
 public class OperationShadow {
@@ -9,6 +13,12 @@ public class OperationShadow {
     public static final String MOD_ID = "operationshadow";
 
     public OperationShadow() {
+
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModBlocks.register(modEventBus);
+        ModItems.register(modEventBus);
+
         MinecraftForge.EVENT_BUS.register(this);
 
         System.out.println("Operation Shadow loaded!");
