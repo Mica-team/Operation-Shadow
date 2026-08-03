b624c87f886f73f50465ff19bc07a56bfbe0e132
jimj90201-a11y <jimj90201@gmail.com>
2026-08-03 19:00:32 +0530

    Remove PlayerSpawnEvent registration from OperationShadow
    Removed registration of PlayerSpawnEvent handler in the constructor.

--- src/main/java/com/gamerofpro/operationshadow/OperationShadow.java
+++ src/main/java/com/gamerofpro/operationshadow/OperationShadow.java
@@ -1,6 +1,5 @@
 package com.gamerofpro.operationshadow;
 
-import com.gamerofpro.operationshadow.event.PlayerSpawnEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 
@@ -10,11 +9,7 @@ public class OperationShadow {
     public static final String MOD_ID = "operationshadow";
 
     public OperationShadow() {
-
         MinecraftForge.EVENT_BUS.register(this);
-
-        // Register PlayerSpawnEvent handler
-        MinecraftForge.EVENT_BUS.register(new PlayerSpawnEvent());
 
         System.out.println("Operation Shadow loaded!");
     }
